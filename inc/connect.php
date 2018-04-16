ec4457d546534715fde8c00b8510b0f7eb29c454
Gabby Rutherford <gabbyrutherford@gmail.com>
2018-04-15 21:53:21 -0500

    Initial Commit

---
+++ inc/connect.php
@@ -0,0 +1,23 @@
+<?php
+
+    $host = '127.0.0.1';
+
+    $username = 'root';
+
+    $password = 'Botan555';
+
+    $database = 'techCo';
+
+    $connection = mysqli_connect($host, $username, $password, $database);
+
+    if ($connection) {
+        
+        echo "<h1 style=\"color: green;\">Connection was successful!</h1>";
+
+    } else {
+        
+        echo "<h1 style=\"color: red;\">Connection was not successful: </h1>" . mysqli_error($e); 
+        
+        die();
+    }
+?>
